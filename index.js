06e4610a5d1d2502d76ec46c695d8e1c153b93cf
dachieu2801 <ankhieu322@gmail.com>
2024-03-21 17:06:50 +0700

    1

---
+++ index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const env = require('./env')
+
+// const port = 5000
+const port = Number(env.PORT) || 5000
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
+
+const cors = require('cors');
+app.use(cors());
+
+//link mongooseDB
+// const db = require('./conf/db');
+// db.main();
+
+//route
+const route = require('./routes');
+route(app);
+
+
+
+app.listen(port)
+
+
+đáad
+ádasds
+ádasd
+ádsa
+đa
